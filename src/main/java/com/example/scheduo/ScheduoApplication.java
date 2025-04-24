d67f82a25e6435b62153e97ffadc4d20ca4fb8b1
g00hyun <rhkdgus1417@hufs.ac.kr>
2025-04-25 01:48:01 +0900

    new project

---
+++ src/main/java/com/example/scheduo/ScheduoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduoApplication.class, args);
+    }
+
+}
