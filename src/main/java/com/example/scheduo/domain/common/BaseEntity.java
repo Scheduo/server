a48ed5860235a3556be0006568ad3947ff283a11
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-04-26 23:03:14 +0900

    :sparkles: String -> LocalDateTime으로 수정

--- src/main/java/com/example/scheduo/domain/common/BaseEntity.java
+++ src/main/java/com/example/scheduo/domain/common/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.example.scheduo.domain.common;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,7 +17,7 @@ import lombok.Getter;
 public abstract class BaseEntity {
 	@CreatedDate
 	@Column(updatable = false)
-	private String createdAt;
+	private LocalDateTime createdAt;
 	@LastModifiedDate
-	private String updatedAt;
+	private LocalDateTime updatedAt;
 }
