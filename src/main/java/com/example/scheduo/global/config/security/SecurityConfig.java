7243c2c83ec7d95fe4c6e802f93bb14237a5f5d1
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-05-14 20:16:25 +0900

    :sparkles: Google 소셜 로그인 구현

---
+++ src/main/java/com/example/scheduo/global/config/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.scheduo.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.example.scheduo.global.auth.CustomOAuth2UserService;
+import com.example.scheduo.global.config.security.handler.CustomOAuth2SuccessHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+					CorsConfiguration config = new CorsConfiguration();
+					config.addAllowedOrigin("http://localhost:3000");
+					config.addAllowedMethod("*");
+					config.addAllowedHeader("*");
+					config.setAllowCredentials(true);
+					return config;
+				}
+			}))
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+				.successHandler(customOAuth2SuccessHandler));
+
+		return http.build();
+	}
+}
