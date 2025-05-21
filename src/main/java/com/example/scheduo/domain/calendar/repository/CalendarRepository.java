b0c7e58c2eaf0abc93e0b126ec24f4eacdf1cccc
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-05-21 21:23:05 +0900

    :sparkles: 회원가입 시 기본 캘린더 자동 생성 로직 추가

---
+++ src/main/java/com/example/scheduo/domain/calendar/repository/CalendarRepository.java
@@ -0,0 +1,8 @@
+package com.example.scheduo.domain.calendar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.scheduo.domain.calendar.entity.Calendar;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+}
