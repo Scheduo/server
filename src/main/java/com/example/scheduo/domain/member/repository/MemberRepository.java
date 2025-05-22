bf97c0adefaad2b56fa426e36cd6b3b03bc39db1
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-05-22 17:37:20 +0900

    :hammer: 이메일 중복 체크 시 socialType 고려하도록 수정

--- src/main/java/com/example/scheduo/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/scheduo/domain/member/repository/MemberRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.scheduo.domain.member.entity.Member;
+import com.example.scheduo.domain.member.entity.SocialType;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findMemberByEmail(String email);
+
+	Optional<Member> findMemberByEmailAndSocialType(String email, SocialType socialType);
 }
