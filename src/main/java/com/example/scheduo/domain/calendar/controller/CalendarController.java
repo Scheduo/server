2a467f5115c0086e81f3f4f4a9e823a3f7c17f36
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-05-23 20:11:08 +0900

    :sparkles: 사용자 초대 API 구현

---
+++ src/main/java/com/example/scheduo/domain/calendar/controller/CalendarController.java
@@ -0,0 +1,29 @@
+package com.example.scheduo.domain.calendar.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.scheduo.domain.calendar.dto.CalendarRequestDto;
+import com.example.scheduo.domain.calendar.service.CalendarService;
+import com.example.scheduo.global.response.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/calendars")
+public class CalendarController {
+	private final CalendarService calendarService;
+
+	@PostMapping("/{calendarId}/invite")
+	public ApiResponse<?> invite(@PathVariable("calendarId") Long calendarId,
+		@RequestBody CalendarRequestDto.Invite request) {
+		//Todo: 추후에 AuthenticationContext에서 memberId를 가져와서 사용하도록 수정
+		Long memberId = 1450L;
+		calendarService.inviteMember(calendarId, memberId, request.getMemberId());
+		return ApiResponse.onSuccess();
+	}
+}
