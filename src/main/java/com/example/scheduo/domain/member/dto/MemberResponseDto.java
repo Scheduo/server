f0e8c989831a076bbfd3cdc9b5ea024ab94202eb
g00hyun <rhkdgus1417@hufs.ac.kr>
2025-05-29 06:08:33 +0900

    :recycle: 객체 생성 builder로 통일

--- src/main/java/com/example/scheduo/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/example/scheduo/domain/member/dto/MemberResponseDto.java
@@ -4,36 +4,40 @@ import java.util.List;
 
 import com.example.scheduo.domain.member.entity.Member;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public class MemberResponseDto {
 	@Getter
-	@RequiredArgsConstructor
+	@Builder
 	public static class MemberInfo {
-		private final Long id;
-		private final String email;
-		private final String nickname;
+		private Long id;
+		private String email;
+		private String nickname;
 
 		public static MemberInfo from(Member member) {
-			return new MemberInfo(
-				member.getId(),
-				member.getEmail(),
-				member.getNickname()
-			);
+			return MemberInfo.builder()
+				.id(member.getId())
+				.email(member.getEmail())
+				.nickname(member.getNickname())
+				.build();
 		}
 	}
 
 	@Getter
-	@RequiredArgsConstructor
+	@Builder
 	public static class MemberList {
-		private final List<MemberInfo> users;
+		private List<MemberInfo> users;
 
 		public static MemberList from(List<Member> members) {
 			List<MemberInfo> profiles = members.stream()
 				.map(MemberInfo::from)
 				.toList();
-			return new MemberList(profiles);
+
+			return MemberList.builder()
+				.users(profiles)
+				.build();
 		}
 	}
 }
