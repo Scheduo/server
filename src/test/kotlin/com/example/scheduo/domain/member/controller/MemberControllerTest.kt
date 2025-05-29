57b622527c59cfeabd2a1b221b1e296d1743c88a
g00hyun <rhkdgus1417@hufs.ac.kr>
2025-05-29 18:42:04 +0900

    :recycle: test profile 설정

--- src/test/kotlin/com/example/scheduo/domain/member/controller/MemberControllerTest.kt
+++ src/test/kotlin/com/example/scheduo/domain/member/controller/MemberControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.patch
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class MemberControllerTest(
         @Autowired val mockMvc: MockMvc,
         @Autowired val objectMapper: ObjectMapper,
--- src/test/kotlin/com/example/scheduo/domain/member/controller/NotificationControllerTest.kt
+++ src/test/kotlin/com/example/scheduo/domain/member/controller/NotificationControllerTest.kt
@@ -11,6 +11,7 @@ import io.kotest.matchers.shouldBe
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.transaction.annotation.Transactional
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class NotificationControllerTest(
         @Autowired val mockMvc: MockMvc,
         @Autowired val objectMapper: ObjectMapper,
