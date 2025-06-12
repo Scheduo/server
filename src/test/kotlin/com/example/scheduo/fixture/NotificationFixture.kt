23cb6ceca447948e8d5fee61b6f84982438344c1
seungheon123 <seungheon.bisb.2012@gmail.com>
2025-06-12 15:17:21 +0900

    :white_check_mark: 중복되는 함수 제거

--- src/test/kotlin/com/example/scheduo/fixture/NotificationFixture.kt
+++ src/test/kotlin/com/example/scheduo/fixture/NotificationFixture.kt
@@ -4,36 +4,6 @@ import com.example.scheduo.domain.member.entity.Member
 import com.example.scheduo.domain.member.entity.Notification
 import com.example.scheduo.domain.member.entity.NotificationType
 
-fun createScheduleNotification(
-    id: Long? = null,
-    member: Member,
-    message: String = "회의 일정이 곧 시작됩니다",
-    data: Map<String, Any> = mapOf("scheduleId" to 45, "calendarId" to 3)
-): Notification {
-    return Notification(
-        id,
-        member,
-        NotificationType.SCHEDULE_NOTIFICATION,
-        message,
-        data
-    )
-}
-
-fun createCalendarInvitationNotification(
-    id: Long? = null,
-    member: Member,
-    message: String = "새로운 캘린더 초대가 도착했습니다",
-    data: Map<String, Any> = mapOf("calendarId" to 7)
-): Notification {
-    return Notification(
-        id,
-        member,
-        NotificationType.CALENDAR_INVITATION,
-        message,
-        data
-    )
-}
-
 fun createNotification(
     id: Long? = null,
     member: Member,
